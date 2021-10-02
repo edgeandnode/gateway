a10d4a1aa327076601a54ce922ae181b2d3b75d3
Theo Butler <theodusbutler@gmail.com>
2021-10-02 18:46:15 -0400

    Sync API keys from agent

--- src/query_engine/tests.rs
+++ src/query_engine/tests.rs
@@ -168,7 +168,7 @@ impl Topology {
         let query = if self.flip_coin(32) { "?" } else { BASIC_QUERY };
         ClientQuery {
             id: self.rng.next_u64(),
-            api_key: "".into(),
+            api_key: None,
             query: query.into(),
             variables: None,
             network,
