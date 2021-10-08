afb99b0737ee06e998f4b9793bc76c3e6a685d9e
Theo Butler <theodusbutler@gmail.com>
2021-10-08 10:37:40 -0400

    Move CLI options to separate module

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
 mod alchemy_client;
 mod indexer_selection;
+mod opt;
 mod prelude;
 mod query_engine;
 mod rate_limiter;
 mod sync_client;
 mod ws_client;
 
-use crate::{indexer_selection::SecretKey, prelude::*, query_engine::*, rate_limiter::*};
+use crate::{indexer_selection::SecretKey, opt::*, prelude::*, query_engine::*, rate_limiter::*};
 use actix_web::{
     dev::ServiceRequest,
     http::{header, StatusCode},
     web, App, HttpRequest, HttpResponse, HttpResponseBuilder, HttpServer,
 };
 use async_trait::async_trait;
-use bip39;
 use eventuals::EventualExt;
 use graphql_client::GraphQLQuery;
-use hdwallet::{self, KeyChain as _};
 use hex;
 use indexer_selection::{IndexerQuery, UnresolvedBlock, UtilityConfig};
 use lazy_static::lazy_static;
@@ -32,111 +31,10 @@ use std::{
         Arc,
     },
 };
-use structopt::StructOpt;
-use structopt_derive::StructOpt;
+use structopt::StructOpt as _;
 use tokio::time::Duration;
 use tree_buf;
 use uuid::Uuid;
-
-#[derive(StructOpt, Debug)]
-struct Opt {
-    #[structopt(
-        help = "Ethereum wallet mnemonic",
-        long = "--mnemonic",
-        env = "MNEMONIC"
-    )]
-    mnemonic: String,
-    #[structopt(
-        help = "URL of gateway agent syncing API",
-        long = "--sync-agent",
-        env = "SYNC_AGENT"
-    )]
-    sync_agent: String,
-    #[structopt(
-        help = "Ethereum provider URLs, format: '<network>=<url>,...'\ne.g. rinkeby=eth-rinkeby.alchemyapi.io/v2/<api-key>",
-        long = "--ethereum-providers",
-        env = "ETHEREUM_PROVIDERS"
-    )]
-    ethereum_proviers: EthereumProviders,
-    #[structopt(
-        help = "Network subgraph URL",
-        long = "--network-subgraph",
-        env = "NETWORK_SUBGRAPH"
-    )]
-    network_subgraph: String,
-    #[structopt(help = "Format log output as JSON", long = "--log-json")]
-    log_json: bool,
-    #[structopt(
-        long = "--indexer-selection-retry-limit",
-        env = "INDEXER_SELECTION_LIMIT",
-        default_value = "5"
-    )]
-    indexer_selection_retry_limit: usize,
-    #[structopt(
-        long = "--query-budget",
-        env = "QUERY_BUDGET",
-        default_value = "0.0005"
-    )]
-    query_budget: GRT,
-    #[structopt(long = "--port", env = "PORT", default_value = "6700")]
-    port: u16,
-    #[structopt(long = "--metrics-port", env = "METRICS_PORT", default_value = "7300")]
-    metrics_port: u16,
-    #[structopt(
-        help = "Duration of IP rate limiting window in seconds",
-        long = "--ip-rate-limit-window",
-        env = "IP_RATE_LIMIT_WINDOW",
-        default_value = "10"
-    )]
-    ip_rate_limit_window_secs: u8,
-    #[structopt(
-        help = "IP rate limit per window",
-        long = "--ip-rate-limit",
-        env = "IP_RATE_LIMIT",
-        default_value = "250"
-    )]
-    ip_rate_limit: u16,
-    #[structopt(
-        help = "Duration of API rate limiting window in seconds",
-        long = "--api-rate-limit-window",
-        env = "API_RATE_LIMIT_WINDOW",
-        default_value = "10"
-    )]
-    api_rate_limit_window_secs: u8,
-    #[structopt(
-        help = "API rate limit per window",
-        long = "--api-rate-limit",
-        env = "API_RATE_LIMIT",
-        default_value = "1000"
-    )]
-    api_rate_limit: u16,
-}
-
-#[derive(Debug)]
-struct EthereumProviders(Vec<(String, String)>);
-
-impl FromStr for EthereumProviders {
-    type Err = String;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let err_usage = "networks syntax: <network>=<url>,...";
-        let pairs = s.split(",").collect::<Vec<&str>>();
-        if pairs.is_empty() {
-            return Err(err_usage.into());
-        }
-        pairs
-            .into_iter()
-            .map(|provider| {
-                let kv = provider.split("=").collect::<Vec<&str>>();
-                if kv.len() != 2 {
-                    return None;
-                }
-                Some((kv[0].into(), kv[1].into()))
-            })
-            .collect::<Option<Vec<(String, String)>>>()
-            .map(|providers| EthereumProviders(providers))
-            .ok_or::<String>(err_usage.into())
-    }
-}
 
 #[actix_web::main]
 async fn main() {
@@ -146,23 +44,6 @@ async fn main() {
     tracing::trace!("{:#?}", opt);
     let gateway_id = Uuid::new_v4();
     tracing::info!(%gateway_id);
-
-    let wallet_seed = bip39::Seed::new(
-        &bip39::Mnemonic::from_phrase(&opt.mnemonic, bip39::Language::English)
-            .expect("Invalid mnemonic"),
-        "",
-    );
-    let signer_key = hdwallet::DefaultKeyChain::new(
-        hdwallet::ExtendedPrivKey::with_seed(wallet_seed.as_bytes()).expect("Invalid mnemonic"),
-    )
-    .derive_private_key(key_path("scalar/allocations").into())
-    .expect("Failed to derive signer key")
-    .0
-    .private_key;
-    // Convert between versions of secp256k1 lib.
-    let signer_key = SecretKey::from_slice(signer_key.as_ref()).unwrap();
-    // Zeroize the wallet seed.
-    drop(wallet_seed);
 
     let (input_writers, inputs) = Inputs::new();
 
@@ -190,6 +71,7 @@ async fn main() {
             ((network, send), metrics)
         })
         .unzip();
+    let signer_key = opt.signer_key.0;
     let (api_keys_writer, api_keys) = Eventual::new();
     let sync_metrics = sync_client::create(
         opt.sync_agent,
---
+++ src/opt.rs
@@ -0,0 +1,130 @@
+use crate::{indexer_selection::SecretKey, prelude::*};
+use bip39;
+use hdwallet::{self, KeyChain as _};
+use std::error::Error;
+use structopt_derive::StructOpt;
+
+#[derive(StructOpt, Debug)]
+pub struct Opt {
+    #[structopt(
+        help = "Ethereum wallet mnemonic",
+        long = "--mnemonic",
+        env = "MNEMONIC"
+    )]
+    pub signer_key: SignerKey,
+    #[structopt(
+        help = "URL of gateway agent syncing API",
+        long = "--sync-agent",
+        env = "SYNC_AGENT"
+    )]
+    pub sync_agent: String,
+    #[structopt(
+        help = "Ethereum provider URLs, format: '<network>=<url>,...'\ne.g. rinkeby=eth-rinkeby.alchemyapi.io/v2/<api-key>",
+        long = "--ethereum-providers",
+        env = "ETHEREUM_PROVIDERS"
+    )]
+    pub ethereum_proviers: EthereumProviders,
+    #[structopt(
+        help = "Network subgraph URL",
+        long = "--network-subgraph",
+        env = "NETWORK_SUBGRAPH"
+    )]
+    pub network_subgraph: String,
+    #[structopt(help = "Format log output as JSON", long = "--log-json")]
+    pub log_json: bool,
+    #[structopt(
+        long = "--indexer-selection-retry-limit",
+        env = "INDEXER_SELECTION_LIMIT",
+        default_value = "5"
+    )]
+    pub indexer_selection_retry_limit: usize,
+    #[structopt(
+        long = "--query-budget",
+        env = "QUERY_BUDGET",
+        default_value = "0.0005"
+    )]
+    pub query_budget: GRT,
+    #[structopt(long = "--port", env = "PORT", default_value = "6700")]
+    pub port: u16,
+    #[structopt(long = "--metrics-port", env = "METRICS_PORT", default_value = "7300")]
+    pub metrics_port: u16,
+    #[structopt(
+        help = "Duration of IP rate limiting window in seconds",
+        long = "--ip-rate-limit-window",
+        env = "IP_RATE_LIMIT_WINDOW",
+        default_value = "10"
+    )]
+    pub ip_rate_limit_window_secs: u8,
+    #[structopt(
+        help = "IP rate limit per window",
+        long = "--ip-rate-limit",
+        env = "IP_RATE_LIMIT",
+        default_value = "250"
+    )]
+    pub ip_rate_limit: u16,
+    #[structopt(
+        help = "Duration of API rate limiting window in seconds",
+        long = "--api-rate-limit-window",
+        env = "API_RATE_LIMIT_WINDOW",
+        default_value = "10"
+    )]
+    pub api_rate_limit_window_secs: u8,
+    #[structopt(
+        help = "API rate limit per window",
+        long = "--api-rate-limit",
+        env = "API_RATE_LIMIT",
+        default_value = "1000"
+    )]
+    pub api_rate_limit: u16,
+}
+
+#[derive(Debug)]
+pub struct SignerKey(pub SecretKey);
+
+impl FromStr for SignerKey {
+    type Err = Box<dyn Error>;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // Wallet seed zeroized on drop
+        let wallet_seed = bip39::Seed::new(
+            &bip39::Mnemonic::from_phrase(s, bip39::Language::English)?,
+            "",
+        );
+        let signer_key = hdwallet::DefaultKeyChain::new(
+            hdwallet::ExtendedPrivKey::with_seed(wallet_seed.as_bytes()).expect("Invalid mnemonic"),
+        )
+        .derive_private_key(key_path("scalar/allocations").into())
+        .expect("Failed to derive signer key")
+        .0
+        .private_key;
+        Ok(SignerKey(
+            // Convert between versions of secp256k1 lib.
+            SecretKey::from_slice(signer_key.as_ref()).unwrap(),
+        ))
+    }
+}
+
+#[derive(Debug)]
+pub struct EthereumProviders(pub Vec<(String, String)>);
+
+impl FromStr for EthereumProviders {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let err_usage = "networks syntax: <network>=<url>,...";
+        let pairs = s.split(",").collect::<Vec<&str>>();
+        if pairs.is_empty() {
+            return Err(err_usage.into());
+        }
+        pairs
+            .into_iter()
+            .map(|provider| {
+                let kv = provider.split("=").collect::<Vec<&str>>();
+                if kv.len() != 2 {
+                    return None;
+                }
+                Some((kv[0].into(), kv[1].into()))
+            })
+            .collect::<Option<Vec<(String, String)>>>()
+            .map(|providers| EthereumProviders(providers))
+            .ok_or::<String>(err_usage.into())
+    }
+}
