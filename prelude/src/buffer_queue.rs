79c79d38cdef070410c2c0d93fd2772eaced2d34
Zac Burns <That3Percent@gmail.com>
2022-09-14 11:03:59 -0500

    ISA: Don't retry an indexer with the same failed block

--- prelude/src/buffer_queue.rs
+++ prelude/src/buffer_queue.rs
@@ -1,11 +1,23 @@
-use tokio::sync::mpsc::{error::SendError, unbounded_channel, UnboundedReceiver, UnboundedSender};
+use {
+    std::sync::Arc,
+    tokio::sync::{
+        mpsc::{error::SendError, unbounded_channel, UnboundedReceiver, UnboundedSender},
+        Notify,
+    },
+};
+
+#[derive(Debug)]
+pub enum Event<T> {
+    Flush(Arc<Notify>),
+    Update(T),
+}
 
 pub struct QueueReader<T> {
-    recv: UnboundedReceiver<T>,
+    recv: UnboundedReceiver<Event<T>>,
 }
 
 pub struct QueueWriter<T> {
-    send: UnboundedSender<T>,
+    send: UnboundedSender<Event<T>>,
 }
 
 impl<T> Clone for QueueWriter<T> {
@@ -18,14 +30,31 @@ impl<T> Clone for QueueWriter<T> {
 
 impl<T> QueueWriter<T> {
     pub fn write(&self, value: T) -> Result<(), SendError<T>> {
-        self.send.send(value)
+        let event = Event::Update(value);
+        self.send.send(event).map_err(|err| {
+            let value = match err {
+                SendError(Event::Update(value)) => value,
+                _ => unreachable!(),
+            };
+            SendError(value)
+        })
+    }
+
+    pub async fn flush(&self) -> Result<(), SendError<()>> {
+        let notify = Arc::new(Notify::const_new());
+        let notify2 = notify.clone();
+        if self.send.send(Event::Flush(notify2)).is_err() {
+            return Err(SendError(()));
+        }
+        notify.notified().await;
+        Ok(())
     }
 }
 
 impl<T> QueueReader<T> {
     /// This method is cancellation safe
     /// https://docs.rs/tokio/latest/tokio/macro.select.html#cancellation-safety
-    pub async fn read(&mut self, buffer: &mut Vec<T>) {
+    pub async fn read(&mut self, buffer: &mut Vec<Event<T>>) {
         // Cancellation safe because UnboundedReceiver::recv is also
         // cancellation safe, and once we get the first item we fill the buffer
         // without awaiting. So, no messages can be lost.
@@ -35,7 +64,7 @@ impl<T> QueueReader<T> {
         }
     }
 
-    pub fn try_read(&mut self, buffer: &mut Vec<T>) {
+    pub fn try_read(&mut self, buffer: &mut Vec<Event<T>>) {
         while let Ok(next) = self.recv.try_recv() {
             buffer.push(next)
         }
