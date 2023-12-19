cb00150e5e81718196ffc05dda97597e82dbd2db
Lorenzo Delgado <lorenzo@edgeandnode.com>
2023-12-19 16:27:39 +0100

    refactor(graph-gateway): extract studio api key parsing logic (II)

--- graph-gateway/src/auth/subscriptions.rs
+++ graph-gateway/src/auth/subscriptions.rs
@@ -1,37 +1,15 @@
-use std::collections::HashMap;
-
-use alloy_primitives::Address;
 use axum::extract::FromRef;
-use eventuals::{Eventual, Ptr};
 use thegraph::subscriptions::auth::{parse_auth_token, verify_auth_token_claims, AuthTokenClaims};
-
-use crate::subscriptions::Subscription;
 
 use super::AuthHandler;
 
 /// App state (a.k.a [Context](crate::client_query::Context)) sub-state.
-pub struct SubscriptionsAuthHandler {
-    /// A map between the Subscription's user address and the actual
-    /// active subscription.
-    ///
-    /// Subscriptions are fetched periodically (every 30s) from the Subscriptions subgraph by
-    /// the gateway using the [`subscriptions_subgraph` client](crate::subscriptions_subgraph::Client).
-    subscriptions: Eventual<Ptr<HashMap<Address, Subscription>>>,
-}
+pub struct SubscriptionsAuthHandler {}
 
 // TODO(LNSD): Use `client_query::Context` instead of `AuthHandler`.
 impl FromRef<AuthHandler> for SubscriptionsAuthHandler {
-    fn from_ref(auth: &AuthHandler) -> Self {
-        Self {
-            subscriptions: auth.subscriptions.clone(),
-        }
-    }
-}
-
-impl SubscriptionsAuthHandler {
-    /// Get the subscription associated with the auth token claims user.
-    pub fn get_subscription(&self, user: &Address) -> Option<Subscription> {
-        self.subscriptions.value_immediate()?.get(user).cloned()
+    fn from_ref(_auth: &AuthHandler) -> Self {
+        Self {}
     }
 }
 
