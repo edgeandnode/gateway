9bce7e8612929f2d920d759f0b63ede3025d9711
Theo Butler <theodusbutler@gmail.com>
2024-01-02 18:37:18 -0500

    fix: switch exchange rate source to Chainlink (#474)
    Chainlink sources are far less error-prone to use than Uniswap TWAP.
    They are also potentially less susceptible to risks associated with low
    liquidity for the Ethereum GRT/USDC pool.

--- gateway-framework/src/network/exchange_rate.rs
+++ gateway-framework/src/network/exchange_rate.rs
@@ -1,7 +1,6 @@
 use std::sync::Arc;
 use std::time::Duration;
 
-use alloy_primitives::U256;
 use anyhow::ensure;
 use ethers::{
     abi::Address,
@@ -9,62 +8,75 @@ use ethers::{
     providers::Provider,
 };
 use eventuals::{Eventual, EventualExt, EventualWriter};
+use indexer_selection::NotNan;
 use tokio::sync::Mutex;
 use toolshed::url::Url;
 
 use gateway_common::types::{UDecimal18, GRT};
 
 abigen!(
-    UniswapV3Pool,
-    "gateway-framework/src/contract_abis/UniswapV3Pool.json",
+    ChainlinkPriceFeed,
+    "gateway-framework/src/contract_abis/ChainlinkPriceFeed.json",
     event_derives(serde::Deserialize, serde::Serialize);
 );
 
 pub async fn grt_per_usd(provider: Url) -> anyhow::Result<Eventual<GRT>> {
-    // https://info.uniswap.org/#/pools/0x4d1fb02fa84eda35881902e8e0fdacc3a873398b
-    let uniswap_v3_pool: Address = "0x4d1Fb02fa84EdA35881902e8E0fdacC3a873398B"
+    // https://data.chain.link/ethereum/mainnet/crypto-eth/grt-eth
+    let chainlink_eth_per_grt: Address = "0x17d054ecac33d91f7340645341efb5de9009f1c1"
         .parse()
         .unwrap();
-    let provider = Arc::new(Provider::<Http>::try_from(provider.to_string()).unwrap());
-    let pool: &'static UniswapV3Pool<Provider<Http>> =
-        Box::leak(Box::new(UniswapV3Pool::new(uniswap_v3_pool, provider)));
+    // https://data.chain.link/ethereum/mainnet/crypto-usd/eth-usd
+    let chainlink_usd_per_eth: Address = "0x5f4ec3df9cbd43714fe2740f5e3616155c5b8419"
+        .parse()
+        .unwrap();
 
-    let usdc: Address = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
-        .parse()
-        .unwrap();
-    let grt: Address = "0xc944E90C64B2c07662A292be6244BDf05Cda44a7"
-        .parse()
-        .unwrap();
-    ensure!(pool.token_0().await.unwrap() == usdc);
-    ensure!(pool.token_1().await.unwrap() == grt);
+    let provider = Arc::new(Provider::<Http>::try_from(provider.to_string()).unwrap());
+    let eth_per_grt: &'static ChainlinkPriceFeed<Provider<Http>> = Box::leak(Box::new(
+        ChainlinkPriceFeed::new(chainlink_eth_per_grt, provider.clone()),
+    ));
+    let usd_per_eth: &'static ChainlinkPriceFeed<Provider<Http>> = Box::leak(Box::new(
+        ChainlinkPriceFeed::new(chainlink_usd_per_eth, provider),
+    ));
 
     let (writer, reader) = Eventual::new();
     let writer: &'static Mutex<EventualWriter<GRT>> = Box::leak(Box::new(Mutex::new(writer)));
     eventuals::timer(Duration::from_secs(60))
         .pipe_async(move |_| async {
-            match fetch_grt_per_usd(pool).await {
-                Err(grt_per_usd_err) => tracing::error!(%grt_per_usd_err),
-                Ok(grt_per_usd) => {
-                    tracing::info!(grt_per_usd = %grt_per_usd.0);
-                    writer.lock().await.write(grt_per_usd);
+            let eth_per_grt = match fetch_price(eth_per_grt).await {
+                Ok(eth_per_grt) => eth_per_grt,
+                Err(eth_per_grt_err) => {
+                    tracing::error!(%eth_per_grt_err);
+                    return;
                 }
             };
+            let usd_per_eth = match fetch_price(usd_per_eth).await {
+                Ok(usd_per_eth) => usd_per_eth,
+                Err(usd_per_eth_err) => {
+                    tracing::error!(%usd_per_eth_err);
+                    return;
+                }
+            };
+            let usd_per_grt = eth_per_grt * usd_per_eth;
+            let grt_per_usd = match UDecimal18::try_from(usd_per_grt.recip()) {
+                Ok(grt_per_usd) => GRT(grt_per_usd),
+                Err(grt_per_usd_err) => {
+                    tracing::error!(%grt_per_usd_err);
+                    return;
+                }
+            };
+            tracing::info!(grt_per_usd = %grt_per_usd.0);
+            writer.lock().await.write(grt_per_usd);
         })
         .forever();
     Ok(reader)
 }
 
-async fn fetch_grt_per_usd(pool: &UniswapV3Pool<Provider<Http>>) -> anyhow::Result<GRT> {
-    const GRT_DECIMALS: u32 = 18;
-    const USDC_DECIMALS: u32 = 6;
-    // https://docs.uniswap.org/contracts/v3/reference/core/interfaces/pool/IUniswapV3PoolDerivedState#observe
-    // token1/token0 -> GRT/USDC
-    let twap_seconds: u32 = 60 * 20;
-    let (tick_cumulatives, _) = pool.observe(vec![twap_seconds, 0]).await?;
-    ensure!(tick_cumulatives.len() == 2);
-    let tick = (tick_cumulatives[1] - tick_cumulatives[0]) / twap_seconds as i64;
-    let price = U256::try_from(1.0001_f64.powi(tick as i32)).unwrap();
-    let shift = U256::from(10_u128.pow(18 - (GRT_DECIMALS - USDC_DECIMALS)));
-    let price = UDecimal18::from_raw_u256(price * shift);
-    Ok(GRT(price))
+async fn fetch_price(contract: &ChainlinkPriceFeed<Provider<Http>>) -> anyhow::Result<NotNan<f64>> {
+    let decimals: u8 = contract.decimals().await?;
+    ensure!(decimals <= 18);
+    let latest_answer: u128 = contract.latest_answer().await?.try_into()?;
+    ensure!(latest_answer > 0);
+    Ok(NotNan::new(
+        latest_answer as f64 * 10.0_f64.powi(-(decimals as i32)),
+    )?)
 }
