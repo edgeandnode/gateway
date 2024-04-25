7402e164d7edfbf63452fc1a2377da9e4c64615f
Lorenzo Delgado <lorenzo@edgeandnode.com>
2024-04-25 15:13:27 +0200

    feat(graph-gateway): add network indexers pois resolver and blocklist
    Signed-off-by: Lorenzo Delgado <lorenzo@edgeandnode.com>

---
+++ graph-gateway/src/network/indexers_poi_blocklist.rs
@@ -0,0 +1,97 @@
+//! This module contains the [`PoiBlocklist`] struct, which is used to block indexers based
+//! on their Proof of Indexing (POI) information.
+//!
+//! Given a list of blocked POIs, the blocklist checks if an indexer reports any of them as public
+//! POIs. If a match is found, the indexer is blocked for the associated deployment ID.
+//!
+//! The blocklist caches the blocklist state for each indexer, so that subsequent checks against the
+//! same indexer are fast. The cached entries are considered expired after a given TTL.
+
+use std::collections::{HashMap, HashSet};
+
+use alloy_primitives::BlockNumber;
+use itertools::Itertools;
+use thegraph_core::types::{DeploymentId, ProofOfIndexing};
+
+use crate::{indexers::public_poi::ProofOfIndexingInfo, network::blocklists::BlockState};
+
+/// A blocklist based on the Proof of Indexing (POI) of indexers.
+pub struct PoiBlocklist {
+    blocklist: HashMap<DeploymentId, HashSet<ProofOfIndexingInfo>>,
+}
+
+impl PoiBlocklist {
+    /// Create a new indexer POI blocklist with the given configuration.
+    pub fn new(conf: HashSet<ProofOfIndexingInfo>) -> Self {
+        // Group the blocked POI info by deployment ID
+        let mut conf_map = HashMap::new();
+        for info in conf {
+            let deployment_id = info.deployment_id;
+            conf_map
+                .entry(deployment_id)
+                .or_insert_with(HashSet::new)
+                .insert(info);
+        }
+
+        Self {
+            blocklist: conf_map,
+        }
+    }
+
+    /// Get a list of POIs metadata that are affected.
+    ///
+    /// If none of the deployments are affected, an empty list is returned. This allows to avoid
+    /// querying the indexer for POIs if none of its deployments is affected.
+    pub fn affected_pois_metadata(
+        &self,
+        deployments: &[DeploymentId],
+    ) -> Vec<(DeploymentId, BlockNumber)> {
+        deployments
+            .iter()
+            .flat_map(|deployment_id| {
+                self.blocklist
+                    .get(deployment_id)
+                    .into_iter()
+                    .flat_map(|pois| pois.iter().map(|poi_info| poi_info.meta()))
+            })
+            .unique()
+            .collect()
+    }
+
+    /// Check if any of the reported POIs are in the blocklist.
+    pub fn check(
+        &self,
+        pois: HashMap<(DeploymentId, BlockNumber), ProofOfIndexing>,
+    ) -> HashMap<DeploymentId, BlockState> {
+        pois.iter()
+            .map(|((deployment_id, block_number), poi)| {
+                let state = self.check_poi(*deployment_id, *block_number, *poi);
+                (*deployment_id, state)
+            })
+            .collect()
+    }
+
+    /// Check if the POI is in the blocklist.
+    fn check_poi(
+        &self,
+        deployment_id: DeploymentId,
+        block_number: BlockNumber,
+        poi: ProofOfIndexing,
+    ) -> BlockState {
+        match self.blocklist.get(&deployment_id) {
+            None => BlockState::Allowed,
+            Some(blocked_pois) => {
+                // Check if the POI is blocked
+                if blocked_pois.iter().any(|blocked| {
+                    blocked.deployment_id == deployment_id
+                        && blocked.block_number == block_number
+                        && blocked.proof_of_indexing == poi
+                }) {
+                    BlockState::Blocked
+                } else {
+                    BlockState::Allowed
+                }
+            }
+        }
+    }
+}
---
+++ graph-gateway/src/network/indexers_poi_resolver.rs
@@ -0,0 +1,84 @@
+//! A resolver for the Proof of Indexing (POI) of indexers.
+//!
+//! The resolver fetches the public POIs of indexers based on the given POIs metadata. It caches the
+//! results of these requests to avoid making the same request multiple times.
+//!
+//! The cache has a TTL of 20 minutes. Entries are considered expired after this time causing the
+//! resolver to fetch the public POIs of the indexer again.
+
+use std::{collections::HashMap, time::Duration};
+
+use alloy_primitives::BlockNumber;
+use thegraph_core::types::{DeploymentId, ProofOfIndexing};
+use url::Url;
+
+use super::ttl_hash_map::TtlHashMap;
+use crate::indexers;
+
+/// The default TTL for cache entries is 20 minutes. Entries are considered expired after this time.
+pub const DEFAULT_CACHE_TLL: Duration = Duration::from_secs(20 * 60); // 20 minutes
+
+/// The number of Public POIs to query in a single request.
+const POIS_QUERY_BATCH_SIZE: usize = 10;
+
+/// A resolver for the Proof of Indexing (POI) of indexers.
+pub struct PoiResolver {
+    client: reqwest::Client,
+    cache: TtlHashMap<Url, HashMap<(DeploymentId, BlockNumber), ProofOfIndexing>>,
+}
+
+impl PoiResolver {
+    /// Create a new [`PoiResolver`] with the given client.
+    ///
+    /// The client is used to make requests to indexers. The resolver caches the results of these
+    /// requests to avoid making the same request multiple times.
+    ///
+    /// By default, the cache has a TTL of 20 minutes, [`DEFAULT_CACHE_TLL`]. Entries are considered
+    /// expired after this time causing the resolver to make a new requests to the indexer.
+    pub fn new(client: reqwest::Client) -> Self {
+        Self {
+            client,
+            cache: TtlHashMap::with_ttl(DEFAULT_CACHE_TLL),
+        }
+    }
+
+    /// Resolve the public POIs of the indexer based on the given POIs metadata.
+    ///
+    /// If the public POIs of the indexer are already in the cache, the resolver returns them.
+    pub async fn resolve_indexer_public_pois(
+        &mut self,
+        indexer_status_url: Url,
+        pois_meta: Vec<(DeploymentId, BlockNumber)>,
+    ) -> HashMap<(DeploymentId, BlockNumber), ProofOfIndexing> {
+        // Check if the indexer public POIs are already in the cache
+        match self.cache.get(&indexer_status_url) {
+            Some(pois) => pois.clone(),
+            None => {
+                // Fetch the public POIs of the indexer
+                let pois = self
+                    .fetch_indexer_public_pois(indexer_status_url.clone(), pois_meta)
+                    .await;
+
+                // Insert the public POIs into the cache
+                self.cache.insert(indexer_status_url, pois.clone());
+
+                pois
+            }
+        }
+    }
+
+    /// Fetch the public POIs of the indexer based on the given POIs metadata.
+    async fn fetch_indexer_public_pois(
+        &self,
+        indexer_status_url: Url,
+        pois_meta: Vec<(DeploymentId, BlockNumber)>,
+    ) -> HashMap<(DeploymentId, BlockNumber), ProofOfIndexing> {
+        indexers::public_poi::merge_queries(
+            self.client.clone(),
+            indexer_status_url,
+            pois_meta,
+            POIS_QUERY_BATCH_SIZE,
+        )
+        .await
+    }
+}
