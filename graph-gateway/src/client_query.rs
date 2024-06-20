9569ebe9fcd71267a1b34fc755765060e3a1e5a8
Theo Butler <theodusbutler@gmail.com>
2024-06-20 07:39:31 -0500

    feat: add TooFarBehind error

--- graph-gateway/src/client_query.rs
+++ graph-gateway/src/client_query.rs
@@ -598,6 +598,14 @@ fn build_candidates_list(
             }
         };
 
+        if block_requirements.latest && (perf.seconds_behind > (60 * 30)) {
+            candidates_errors.insert(
+                indexing_id.indexer,
+                IndexerError::Unavailable(UnavailableReason::TooFarBehind),
+            );
+            continue;
+        }
+
         // Check if the indexer is within the required block range
         if let Some((min_block, max_block)) = &block_requirements.range {
             // Allow indexers if their last reported block is "close enough" to the required block
