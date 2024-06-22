d0e3345d164377f780ad61550b3dad9d33aa9526
Theo Butler <theodusbutler@gmail.com>
2024-06-22 11:45:13 -0500

    remove unnecessary type params

--- graph-gateway/src/network/internal.rs
+++ graph-gateway/src/network/internal.rs
@@ -6,15 +6,22 @@ use thegraph_core::types::{DeploymentId, SubgraphId};
 use self::indexer_processing::IndexerRawInfo;
 pub use self::{
     snapshot::{Indexer, Indexing, IndexingId, IndexingProgress, NetworkTopologySnapshot},
-    state::InternalState,
     subgraph_processing::{AllocationInfo, DeploymentInfo, SubgraphInfo, SubgraphVersionInfo},
 };
-use super::{subgraph_client::Client as SubgraphClient, DeploymentError, SubgraphError};
+use super::{
+    config::VersionRequirements, indexer_addr_blocklist::AddrBlocklist,
+    indexer_host_blocklist::HostBlocklist, indexer_host_resolver::HostResolver,
+    indexer_indexing_cost_model_compiler::CostModelCompiler,
+    indexer_indexing_cost_model_resolver::CostModelResolver,
+    indexer_indexing_poi_blocklist::PoiBlocklist, indexer_indexing_poi_resolver::PoiResolver,
+    indexer_indexing_progress_resolver::IndexingProgressResolver,
+    indexer_version_resolver::VersionResolver, subgraph_client::Client as SubgraphClient,
+    DeploymentError, SubgraphError,
+};
 
 mod indexer_processing;
 mod pre_processing;
 mod snapshot;
-mod state;
 mod subgraph_processing;
 
 /// Fetch the network topology information from the graph network subgraph.
@@ -35,6 +42,17 @@ pub struct PreprocessedNetworkInfo {
     subgraphs: HashMap<SubgraphId, Result<SubgraphInfo, SubgraphError>>,
     deployments: HashMap<DeploymentId, Result<DeploymentInfo, DeploymentError>>,
     indexers: HashMap<Address, IndexerRawInfo>,
+}
+
+pub struct InternalState {
+    pub indexer_addr_blocklist: Option<AddrBlocklist>,
+    pub indexer_host_resolver: HostResolver,
+    pub indexer_host_blocklist: Option<HostBlocklist>,
+    pub indexer_version_requirements: VersionRequirements,
+    pub indexer_version_resolver: VersionResolver,
+    pub indexer_indexing_pois_blocklist: Option<(PoiResolver, PoiBlocklist)>,
+    pub indexer_indexing_progress_resolver: IndexingProgressResolver,
+    pub indexer_indexing_cost_model_resolver: (CostModelResolver, CostModelCompiler),
 }
 
 /// Fetch the subgraphs information from the graph network subgraph and performs pre-processing
