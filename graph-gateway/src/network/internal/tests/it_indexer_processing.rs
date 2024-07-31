4ab12704d5ec07f4e74180e1c630ed0bab348b93
Theo Butler <theodusbutler@gmail.com>
2024-07-31 10:14:22 -0400

    chore: replace TtlHashMap uses with max duration

--- graph-gateway/src/network/internal/tests/it_indexer_processing.rs
+++ graph-gateway/src/network/internal/tests/it_indexer_processing.rs
@@ -25,14 +25,13 @@ use crate::network::{
     indexer_host_blocklist::HostBlocklist,
     indexer_host_resolver::HostResolver,
     indexer_indexing_cost_model_compiler::CostModelCompiler,
-    indexer_indexing_cost_model_resolver::CostModelResolver,
+    indexer_indexing_cost_model_resolver::{
+        CostModelResolver, DEFAULT_INDEXER_INDEXING_COST_MODEL_RESOLUTION_TIMEOUT,
+    },
     indexer_indexing_poi_blocklist::PoiBlocklist,
     indexer_indexing_poi_resolver::PoiResolver,
     indexer_indexing_progress_resolver::IndexingProgressResolver,
-    indexer_version_resolver::{
-        VersionResolver, DEFAULT_INDEXER_VERSION_CACHE_TTL,
-        DEFAULT_INDEXER_VERSION_RESOLUTION_TIMEOUT,
-    },
+    indexer_version_resolver::{VersionResolver, DEFAULT_INDEXER_VERSION_RESOLUTION_TIMEOUT},
 };
 
 // Test method to initialize the tests tracing subscriber.
@@ -84,15 +83,17 @@ fn test_service_state(
 ) -> InternalState {
     let indexers_http_client = reqwest::Client::new();
     let indexer_host_resolver = HostResolver::new().expect("Failed to create host resolver");
-    let indexer_version_resolver = VersionResolver::with_timeout_and_cache_ttl(
+    let indexer_version_resolver = VersionResolver::new(
         indexers_http_client.clone(),
         DEFAULT_INDEXER_VERSION_RESOLUTION_TIMEOUT, // 1500 ms
-        DEFAULT_INDEXER_VERSION_CACHE_TTL,          // 20 minutes
     );
     let indexer_indexing_progress_resolver =
         IndexingProgressResolver::new(indexers_http_client.clone(), Duration::from_secs(60));
     let indexer_indexing_cost_model_resolver = (
-        CostModelResolver::new(indexers_http_client.clone()),
+        CostModelResolver::new(
+            indexers_http_client.clone(),
+            DEFAULT_INDEXER_INDEXING_COST_MODEL_RESOLUTION_TIMEOUT,
+        ),
         CostModelCompiler::default(),
     );
 
